a0ad078144035ac1e71c1e61b441b1ce9803465d
usc-stl004 <xJLsRzs57h7a7R8>
2019-10-21 15:03:54 +1000

    gte button not working look at web service

--- week_10_Booklist_web_service.py
+++ week_10_Booklist_web_service.py
@@ -1,20 +1,20 @@
 from bottle import route, run, request, response
 from xml.dom import minidom, Node
 
-@route('/getroute/<planeroute>')
-def get_route(planeroute):
-    print(planeroute)
-    DOMTree = minidom.parse("planeroute.xml")
+@route('/getRoute/<routeName>')
+def get_Route(routeName):
+    print(routeName)
+    DOMTree = minidom.parse(routeName + ".xml")
 
-    route = DOMTree.getElementsByTagName("book")
+    route = DOMTree.getElementsByTagName("route")
     for child in DOMTree.childNodes:
         if child.nodeType == 1:
             for child2 in child.childNodes:
                 if child2.nodeType == 1:
                     for child3 in child2.childNodes:
                         if child3.nodeType == 1:
-                            if child3.nodeName == "title":
-                                if child3.childNodes[0].nodeValue == planeroute:
+                            if child3.nodeName == "route":
+                                if child3.childNodes[0].nodeValue == routeName:
                                     print(child3.parentNode.toxml())
                                     route = child3.parentNode.toxml()
 
@@ -24,8 +24,6 @@ def get_route(planeroute):
 
 @route('/saveRoute')
 def save_route():
-
-
     routes = request.query.route
     print(routes)
     DOMTree = minidom.parseString(routes)
@@ -38,19 +36,14 @@ def save_route():
                 if child2.nodeType == 1:
                     for child3 in child2.childNodes:
                         if child3.nodeType == 1:
-                            print("   ", child3.nodeName, ", ", child3.childNodes[0].nodeValue)
-                            if child3.nodeName == "LatLon":
+                            if child3.nodeName == "latLng":
                                 aArray = (child3.childNodes[0].nodeValue)
-                                Latitude = minidom.parseString("<Latitude>%s</Latitude>" % aArray[0]).documentElement
-                                Longitude = minidom.parseString("<Longitude>%s</Longitude>" % aArray[1]).documentElement
-                                child3.parentNode.appendChild(Latitude)
-                                child3.parentNode.appendChild(Longitude)
+                                latitude = minidom.parseString("<Latitude>%s</Latitude>" % aArray[0]).documentElement
+                                longitude = minidom.parseString("<Longitude>%s</Longitude>" % aArray[1]).documentElement
+                                child3.parentNode.appendChild(latitude)
+                                child3.parentNode.appendChild(longitude)
 
-    fileName = "routeName"  # change to route name in the future
-    with open(fileName + '.xml', 'w') as f:
-        f.write(DOMTree.toxml())
-
-
+    #fileName = "routeName"  # change to route name in the future
     with open(fileName + '.xml', 'w') as f:
         f.write(DOMTree.toxml())
 
