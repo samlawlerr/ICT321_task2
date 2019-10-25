a2065ef7adf98cb964dd3597f80ebda8bda4e75e
usc-stl004 <xJLsRzs57h7a7R8>
2019-10-25 10:00:47 +1000

    fucking done

--- microservice.py
+++ microservice.py
@@ -1,45 +1,63 @@
-from bottle import route, run, request
+from bottle import route, run, request, response
+from xml.dom import minidom, Node
 
-@route('/hello')
-def hello():
-    return "Hello World!"
-
-@route('/')
-def index():
-    """ Display welcome & instruction messages """
-    return "<p>Welcome to my extra simple bottle.py powered server !</p> \
-    	   <p>The Web service can convert a string into UPPERCASE or lowercase. \
-    	   There are two ways to invoke the web service :\
-	   <ul> \
-	      <li>http://localhost:8080/convert/action_name/your string</li> \
-              <li>http://localhost:8080/convert?action=action_name&sentence=your string</li>\
-	   </ul> \
-	   action_name can be UPPER or lower."
+def get_lat_lon(latLng):
+    print("latLng: %s" % latLng)
+    a = latLng.replace(",", "")
+    latLngArray = a.split()
+    return latLngArray
 
 
-@route('/convert/<action>/<sentence>')
-def conversion(action, sentence):
-    if action == "UPPER":
-        sentence = sentence.upper()
-    elif action == "lower":
-        sentence = sentence.lower()
-    else:
-        sentence = "There is something wrong with the service call."
+@route('/getRoute/<routeName>')
+#get route function
+def get_Route(routeName):
+    print(routeName)
 
-    return sentence
+    DOMTree = minidom.parse(routeName + ".xml")
+    routes = DOMTree.getElementsByTagName("routes")
+    print(routeName + ".xml")
 
-@route('/convert')
-def conversion_parameters():
-    action = request.query.action
-    sentence = request.query.sentence
-    if action == "UPPER":
-        sentence = sentence.upper()
-    elif action == "lower":
-        sentence = sentence.lower()
-    else:
-        sentence = "There is something wrong with the service call."
+    for routte in routes:
+        for child in routte.childNodes:
+            if child.nodeType == 1:
+                if child.nodeType == "routeName":
+                    print(routte.toxml())
+                    reroute = routte.toxml()
 
-    return sentence
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Content-type'] = 'application/xml'
+
+    return routes[0].toxml()
+
+@route('/saveRoute')
+#save route function
+def save_route():
+    routes = request.query.route
+    print(routes)
+    DOMTree = minidom.parseString(routes)
+    fileName = request.query.routeName
+    print(fileName)
+
+    for child in DOMTree.childNodes:
+        if child.nodeType == 1:
+            for child2 in child.childNodes:
+                if child2.nodeType == 1:
+                    print("here")
+                    for child3 in child2.childNodes:
+                        if child3.nodeType == 1:
+                            print("here3")
+                            for child4 in child3.childNodes:
+                                if child4.nodeName == "latlng":
+                                # print(child3.nodeName)
+                                    print("in child 3")
+                                    aArray = get_lat_lon(child4.childNodes[0].nodeValue)
+                                    latitude = minidom.parseString("<Latitude>%s</Latitude>" % aArray[0]).documentElement
+                                    longitude = minidom.parseString("<Longitude>%s</Longitude>" % aArray[1]).documentElement
+                                    child4.parentNode.appendChild(latitude)
+                                    child4.parentNode.appendChild(longitude)
+
+    with open(fileName + '.xml', 'w') as f:
+        f.write(DOMTree.toxml())
 
 
 run(host='localhost', port=8080, debug=True)
